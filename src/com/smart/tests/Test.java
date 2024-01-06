70263f144e927565df2dff03dfe434d5a728ebca
jing039 <119808368+jing039@users.noreply.github.com>
2024-01-06 13:18:06 -0500

    Update Test.java with a comment

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	
 	@org.testng.annotations.Test
 	public void loginTest() {
+		// Adding a comment.
 		System.out.println("We are learning Git and GitHub through Eclipse.");
 		System.out.println("We had out initial commit.");
 		
