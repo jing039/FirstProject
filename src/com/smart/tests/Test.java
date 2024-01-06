648e47b2d37c2dd8bac4ad326828779d514fa795
jing039 <tjinghua39@gmail.com>
2024-01-06 12:12:28 -0500

    Initial Commit.

---
+++ src/com/smart/tests/Test.java
@@ -0,0 +1,11 @@
+package com.smart.tests;
+
+public class Test {
+	
+	@org.testng.annotations.Test
+	public void loginTest() {
+		System.out.println("We are learning Git and GitHub through Eclipse.");
+		
+	}
+
+}
